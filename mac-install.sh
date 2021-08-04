86d49149f89aab31aa32447b7122f736ca44b35a
Mo3et <Monet_lee@163.com>
2021-08-05 02:39:57 +0800

    update

---
+++ mac-install.sh
@@ -0,0 +1,50 @@
+#!!! WARNING:  The config is copy by Ou. That only use for refers!! You MUST be modify this config for you.
+
+# WARNING: PLEASE DO NOT USE THIS SCRIPT, JUST MANUALLY FOLLOW THE STEPS
+
+# basic development environment
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew install
+brew tap mongodb/brew
+brew install wget python3 tree macvim node youtube-dl tmux pandoc cmake reattach-to-user-namespace mongodb-community redis httpie vegeta kubectl doctl
+# nodejs install
+npm install -g typescript
+# python install
+pip3 install virtualenv scipy numpy pandas jupyter tensorflow scikit-learn matplotlib seaborn pillow
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mv ~/.zshrc ~/.zshrc_old
+ln zsh/zshrc-mac.conf ~/.zshrc
+ln zsh/changkun.zsh-theme ~/.oh-my-zsh/themes/
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+source ~/.zshrc
+
+# install golang
+mkdir -p ~/goes
+cd ~/goes
+wget https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz
+tar xvf go1.15.6.linux-amd64.tar.gz && rm go1.15.6.linux-amd64.tar.gz
+mv ~/goes/go ~/goes/go1.15
+ln -s ~/goes/go1.15 ~/goes/go
+source ~/.zshrc
+go install changkun.de/x/cmd/{rmtrash,tli}
+
+# install vim config
+ln vim/vimrc.config ~/.vimrc
+mkdir -p ~/.vim/colors/
+ln vim/colors/jellybeans.vim ~/.vim/colors/jellybeans.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+~/.vim/plugged/YouCompleteMe/install.py --go-completer
+
+# install tmux config
+ln tmux/tmux.conf ~/.tmux.conf
+ln tmux/tmux.local.conf ~/.tmux.conf.local
+
+# fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh && cd .. && rm -rf fonts
+
---
+++ ubuntu-install-list.sh
@@ -0,0 +1,58 @@
+#!!! WARNING:  The config is copy by Ou. That only use for refers!! You MUST be modify this config for you.
+
+# this script setup my personal ubuntu setup
+# the setup workflow has been used for the following purpose:
+# - digital ocean
+# - personal desktop
+# - perf work desktop
+
+## install basic tools, grouped line by line
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ssh git vim zsh tmux docker.io
+sudo apt install -y build-essential clang cmake libboost-dev libssl-dev
+sudo apt install -y linux-tools-$(uname -r) linux-tools-generic graphviz
+sudo apt install -y redis wine64
+sudo apt install -y texlive-full pandoc nodejs npm 
+sudo apt install -y httpie vegeta kubectl doctl hugo
+sudo apt install -y xbindkeys xbindkeys-config xclip
+sudo apt install -y tree youtube-dl
+
+## install `oh-my-zsh`
+cd ~/dev/dotfiles
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mv ~/.zshrc ~/.zshrc_old
+ln zsh/zshrc-linux.conf ~/.zshrc
+ln zsh/changkun.zsh-theme ~/.oh-my-zsh/themes/
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+source ~/.zshrc
+
+## install python
+apt install -y python3-pip
+pip3 install virtualenv scipy numpy pandas jupyter tensorflow scikit-learn matplotlib seaborn pillow pyyaml requests
+
+# install golang
+
+# is So outdated   !! 
+mkdir -p ~/goes && cd ~/goes
+wget https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz
+tar xvf go1.15.6.linux-amd64.tar.gz && rm go1.15.6.linux-amd64.tar.gz
+mv ~/goes/go ~/goes/go1.15
+ln -s ~/goes/go1.15 ~/goes/go
+
+
+# install vim config
+cd ~/dev/dotfiles
+ln vim/vimrc.config ~/.vimrc
+mkdir -p ~/.vim/colors/
+ln vim/colors/jellybeans.vim ~/.vim/colors/jellybeans.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+~/.vim/plugged/YouCompleteMe/install.py --go-completer
+
+# install tmux config
+ln tmux/tmux.conf ~/.tmux.conf
+ln tmux/tmux.local.conf ~/.tmux.conf.local
+
+# fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh && cd .. && rm -rf fonts
